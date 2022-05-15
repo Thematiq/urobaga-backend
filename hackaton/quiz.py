ad0a6ae727531b97772db50021c520db099b43f9
Piotr Rzeźnik <piotr.rz01@gmail.com>
2022-05-15 11:58:18 +0200

    working quiz

--- hackaton/quiz.py
+++ hackaton/quiz.py
@@ -12,24 +12,55 @@ def f(x):
 
 class Quiz:
     def __init__(self):
+        self.data = dict()
         with open('quiz_questions/questions.csv') as csvfile:
-            self.data = list(map(f, enumerate(csv.DictReader(csvfile))))
-            self.data = dict(map(lambda x: (x.id, x), self.data))
-            self.data = dict()
-    def get_question(self, key):
-        return self.data[key]
+            data = list(map(f, enumerate(csv.DictReader(csvfile))))
+            self.difficulties = set()
+            for row in data:
+                self.difficulties.add(row.difficulty)
+            self.difficulties = sorted(list(self.difficulties),reverse=True)
+            for level in self.difficulties:
+                self.data[level] = dict()
+
+            for row in data:
+                self.data[row.difficulty][row.id] = row
+
+            print(self.data)
+
+
+    def get_question(self, difficulty, key):
+        return self.data[difficulty][key]
 
 
 class GameQuiz:
     def __init__(self, quiz: Quiz):
         self.quiz = quiz
-        self.available_questions = list(self.quiz.data.keys())
-
-    def get_question(self):
-        question_number = random.choice(self.available_questions)
-        self.available_questions.remove(question_number)
-        return self.quiz.get_question(question_number)
+        self.available_questions = dict()
+        for difficulty in self.quiz.difficulties:
+            self.available_questions[difficulty] = list(self.quiz.data[difficulty].keys())
 
 
+    def get_max_difficulty(self, points):
+        for difficulty in self.quiz.difficulties:
+            if difficulty > points:
+                continue
+            if len(self.available_questions[difficulty]) ==0:
+                continue
+            return difficulty
 
-Quiz()
+    def get_questions(self, points):
+        questions = []
+        while points !=0:
+            difficulty = self.get_max_difficulty(points)
+            points -= difficulty
+            question_number = random.choice(self.available_questions[difficulty])
+            self.available_questions[difficulty].remove(question_number)
+            questions.append(self.quiz.get_question(difficulty, question_number))
+
+        return questions
+
+
+
+quiz = Quiz()
+
+print(GameQuiz(quiz).get_questions(13))
